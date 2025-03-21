6ddf026a2952e7b547bb8fdd31e306fe94648b69
hakyoon_kim <hakyoon_kim@tmax.co.kr>
2025-03-22 00:56:01 +0900

    [feat] router에서 진입하는 프로젝트 코드스플리팅 되도록처리 - 코드 스플리팅 추가

--- src/components/ProjectRouter.tsx
+++ src/components/ProjectRouter.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProjectNavigator from './ProjectNavigator';
 import Header from './Header';
-import StopWatch from '../stopwatch/components/Stopwatch';
-import CoinAppWrapper from '../bitCoinChart/components/CoinAppWrapper';
-import CoinApp from '../bitCoinChart/components/CoinApp';
-import CoinChartView from '../bitCoinChart/components/CoinChartView';
 import { isMobile } from 'react-device-detect';
+import { lazy, Suspense } from 'react';
 
+
+// 진입률이 낮을거 같아서 스플리팅
+const StopWatch = lazy(() => import('../stopwatch/components/Stopwatch'));
+
+// chart는 대부분 웹소켓 웹워커 쉐어드워커 등 무거운 작업을 하므로 스플리팅
+const CoinAppWrapper = lazy(() => import('../bitCoinChart/components/CoinAppWrapper'));
+const CoinApp = lazy(() => import('../bitCoinChart/components/CoinApp'));
+const CoinChartView = lazy(() => import('../bitCoinChart/components/CoinChartView'));
 /**
  * 라우터
  */
@@ -15,14 +20,16 @@ const ProjectRouter = () => {
         <BrowserRouter basename='/hy_toy/'>
             <div className='wrapper'>
                 <Header />
-                <Routes>
-                    <Route path='/' element={<ProjectNavigator/>}/>
-                    <Route path='/stopwatch' element={<StopWatch/>}/>
-                    <Route path='/chart' element={<CoinAppWrapper/>}>
-                        <Route index element={<CoinApp />} />
-                        {isMobile && <Route path="chartview" element={<CoinChartView />} />}
-                    </Route>
-                </Routes>
+                <Suspense fallback={<div>페이지 로딩 중...</div>}>
+                    <Routes>
+                        <Route path='/' element={<ProjectNavigator/>}/>
+                        <Route path='/stopwatch' element={<StopWatch/>}/>
+                        <Route path='/chart' element={<CoinAppWrapper/>}>
+                            <Route index element={<CoinApp />} />
+                            {isMobile && <Route path="chartview" element={<CoinChartView />} />}
+                        </Route>
+                    </Routes>
+                </Suspense>
             </div>
         </BrowserRouter>
     );
