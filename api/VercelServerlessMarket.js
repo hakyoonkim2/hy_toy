dbc2bdf728e242e436b4828e91c6b4eb7e532d65
hakyoon_kim <hakyoon_kim@tmax.co.kr>
2025-03-24 02:03:35 +0900

    [bugfix] 배포환경 빌드 에러수정 - vercelServerLessMarket은 js로 변경 @vercel/node 추가시 타입 에러 추가발생 - @types/node추가

--- api/VercelServerlessMarket.js
+++ api/VercelServerlessMarket.js
@@ -1,13 +1,10 @@
 // api/markets.js
-// vercel 서버리스 function 용
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
     const apiUrl = 'https://api.upbit.com/v1/market/all?isDetails=true';
 
     const response = await fetch(apiUrl);
-
+    
     if (!response.ok) {
       res.status(response.status).json({ error: 'API Error' });
       return;
@@ -17,7 +14,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     res.setHeader('Access-Control-Allow-Origin', '*'); // CORS 허용
     res.status(200).json(data);
-  } catch (error: any) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
