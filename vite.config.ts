0d0bb73e3a655124ecfbc0c15df19864b4c80a90
hakyoon_kim <hakyoon_kim@tmax.co.kr>
2025-03-24 00:25:19 +0900

    [chore] 환경변수 주입 - 환경변수 주입: vercel에서는 해당 값을 사이트옵션으로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfigExport } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-// 환경변수로 배포 대상 구분
-const isGitHub = process.env.VITE_DEPLOY_TARGET === 'GH';
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+    // 1. 환경 변수 로드
+    const env = loadEnv(mode, process.cwd());
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: isGitHub ? '/hy_toy/' : '/',
-  build: {
-    outDir: 'dist'
-  },
-  plugins: [react(), svgr(), {
-    name: 'html-transform',
-    transformIndexHtml(html) {
-      const version = Date.now(); // 또는 `git rev-parse --short HEAD`
-      return html.replace(/style.scss/g, `style.css?v=${version}`)
-                 .replace(/script.js/g, `script.js?v=${version}`);
-    },
-  }],
-  server: {
-    host: true,
-    port: 5173,
-  },
-})
+    // 2. 환경 변수 접근 (VITE_ 접두어 반드시 필요)
+    const isGitHub = env.VITE_DEPLOY_TARGET === 'GH';
+
+    return defineConfig({
+      base: isGitHub ? '/hy_toy/' : '/',
+      build: {
+        outDir: 'dist'
+      },
+      plugins: [react(), svgr(), {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          const version = Date.now(); // 또는 `git rev-parse --short HEAD`
+          return html.replace(/style.scss/g, `style.css?v=${version}`)
+                     .replace(/script.js/g, `script.js?v=${version}`);
+        },
+      }],
+      server: {
+        host: true,
+        port: 5173,
+      },
+    })
+}
+
+
