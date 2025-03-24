13b405a5a069be428bf37618c470a70fffdeeeda
hakyoonkim2 <hyoonchild@gmail.com>
2025-03-24 15:24:19 +0900

    [bugfix] 미국에서 업비트 웹소켓연결 제한되는 문제 수정 - 미국의 경우 업비트 웹소켓연결을 허용하지 않기 때문에 US인 Ip를 통해 국가 확인후 us가 아닌경우 웹소켓으로 연결 - 미국이 아닌경우 rest api를 통한 polling 방식으로 처리 (1초단위)

--- src/bitCoinChart/worker/WorkerUtils.ts
+++ src/bitCoinChart/worker/WorkerUtils.ts
@@ -1,21 +1,16 @@
-import { BinanceTickerData } from "./binance/BinanceWorkerTypes";
-import { PriceMap } from "./CoinCommonTypes";
-import { UpbitTickerData } from "./upbit/UpbitWorkerTypes";
+import { BinanceTickerData } from './binance/BinanceWorkerTypes';
+import { PriceMap } from './CoinCommonTypes';
+import { UpbitRestTicker, UpbitTickerData } from './upbit/UpbitWorkerTypes';
 
 export function dataSetting<T extends BinanceTickerData | UpbitTickerData>(
   symbolFilterArr: T[],
-  priceMap: PriceMap,
+  priceMap: PriceMap
 ) {
   symbolFilterArr.forEach((x) => {
     const { symbol, price: curPrice, openPrice } = getSymbolAndPrice(x);
 
     if (priceMap[symbol]) {
-      let color = "#FFFFFF";
-      if (priceMap[symbol].openPrice < curPrice) {
-        color = "#f75467";
-      } else if (priceMap[symbol].openPrice > curPrice) {
-        color = "#4386f9";
-      }
+      const color = getPriceColor(priceMap[symbol].openPrice, curPrice);
       priceMap[symbol].price = curPrice;
       priceMap[symbol].color = color;
       if (openPrice) {
@@ -30,15 +25,59 @@ function getSymbolAndPrice(item: BinanceTickerData | UpbitTickerData): {
   price: number;
   openPrice?: number;
 } {
-  if ("s" in item && "c" in item) {
+  if ('s' in item && 'c' in item) {
     return { symbol: item.s, price: parseFloat(item.c) }; // Binance
-  } else if ("code" in item && "trade_price" in item) {
+  } else if ('code' in item && 'trade_price' in item) {
     return {
       symbol: item.code,
       price: item.trade_price,
       openPrice: item.opening_price,
     }; // Upbit
   } else {
-    throw new Error("Unknown data type");
+    throw new Error('Unknown data type');
   }
+}
+
+export function getPriceColor(openPrice: number, curPrice: number): string {
+  let color = '#FFFFFF';
+  if (openPrice < curPrice) {
+    color = '#f75467';
+  } else if (openPrice > curPrice) {
+    color = '#4386f9';
+  }
+  return color;
+}
+
+export async function findIpContry(): Promise<boolean> {
+  try {
+    const res = await fetch('https://ipapi.co/json/');
+    const { country_code } = await res.json();
+    return country_code !== 'US';
+  } catch (error) {
+    console.error('Failed to detect country from IP:', error);
+    return false; // 에러 발생 시 기본적으로 차단 처리
+  }
+}
+function chunkMarkets(markets: string[], size: number): string[][] {
+  const chunks = [];
+  for (let i = 0; i < markets.length; i += size) {
+    chunks.push(markets.slice(i, i + size));
+  }
+  return chunks;
+}
+
+async function fetchTickerChunk(markets: string[]) {
+  const query = markets.join(',');
+  const apiUrl = encodeURIComponent(`https://api.upbit.com/v1/ticker?markets=${query}`);
+
+  const res = await fetch(`https://proxy-server-flax-rho.vercel.app/api/proxy?url=${apiUrl}`);
+  return res.json();
+}
+
+export async function fetchAllTickers(allMarkets: string[]): Promise<UpbitRestTicker[]> {
+  const chunks = chunkMarkets(allMarkets, 100);
+
+  const allResults = await Promise.all(chunks.map(fetchTickerChunk));
+
+  return allResults.flat();
 }
