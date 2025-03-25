0d68fe18ed18504ac345957486337b99749e0532
hakyoonkim2 <hyoonchild@gmail.com>
2025-03-26 03:52:18 +0900

    [bugfix] 프로젝트 이름 출력 오류 수정 - 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     navigate('/');
   };
 
-  const currentProject = window.location.href.replace(window.location.origin, '').split('/').at(0);
+  const currentProject = window.location.href.replace(window.location.origin, '').split('/').at(1);
 
   return (
     <header>
