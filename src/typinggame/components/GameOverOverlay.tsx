564870612a46a37004c25311ba73f28e0f35d7b7
hakyoonkim2 <hyoonchild@gmail.com>
2025-04-07 16:30:47 +0900

    [feat] TypingGame 랭킹 시스템 추가 - firebase에 typing collection 추가(read true, write auth가 있을때만 true - gameover시 Top5  랭킹 출력되고 자신의 랭킹은 highlight, 만약 Top 5안에 자신의 점수가 없는 경우 현재 점수가 몇등인지 출력해주도록 처리 - 로그인된 상태가 아닌경우 현재 몇등인지 출력후 랭킹 등록 버튼을 눌러 login 유도 - 로그인이 완료되면 랭킹 보드에 반영된 현재 점수 순위를 보여줌

--- src/typinggame/components/GameOverOverlay.tsx
+++ src/typinggame/components/GameOverOverlay.tsx
@@ -1,16 +1,19 @@
+import GameRanking from '@typinggame/components/GameRanking';
 import style from '@typinggame/styles/GameOverOverlay.module.scss';
 
 type Props = {
   restartGame: () => void;
+  score: number;
 };
 
 /**
  * 게임 오버 UI
  */
-const GameOverOverlay = ({ restartGame }: Props) => {
+const GameOverOverlay = ({ restartGame, score }: Props) => {
   return (
     <div className={style.gameOverOverlay}>
       <p>Game Over</p>
+      <GameRanking score={score} />
       <button onClick={restartGame} className={style.restartButton}>
         Restart
       </button>
